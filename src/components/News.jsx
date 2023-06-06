147900ab3911128642bce59ae79a25643d2aa00e
Yash <yashvdkd@gmail.com>
2023-06-07 00:53:04 +0530

    feat: infinite scroll added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItems from "./NewsItems"
 import Spinner from "../components/Spinner"
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
   static defaultProps = {
@@ -19,8 +20,9 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false,
-      page: 1
+      loading: true,
+      page: 1,
+      totalResults: 0
     }
   }
 
@@ -53,21 +55,38 @@ export class News extends Component {
     this.updateNews();
   }
 
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7ad68a3123654c17b9caf03019dbd6c6&page=${this.state.page}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      totalResults: this.state.articles.concat(parseData.totalResults),
+      articles: parseData.articles,
+    })
+  };
+
   render() {
     return (
       <>
         {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className='px-40 py-20 flex flex-col space-y-4'>
+            {this.state.articles.map((e) => {
+              return <NewsItems key={e.url} title={e.title} description={e.description} imgUrl={e.urlToImage} newsUrl={e.url} source={e.source.name} date={e.publishedAt} />
+            })}
+          </div>
+        </InfiniteScroll>
 
-        <div className='px-40 py-20 flex flex-col space-y-4'>
-          {!this.state.loading && this.state.articles.map((e) => {
-            return <NewsItems key={e.url} title={e.title} description={e.description} imgUrl={e.urlToImage} newsUrl={e.url} source={e.source.name} date={e.publishedAt} />
-          })}
-        </div>
-
-        <div className="p-20 flex justify-evenly">
+        {/* <div className="p-20 flex justify-evenly">
           <button disabled={this.state.page <= 1} className='py-2 px-4 bg-zinc-800 text-zinc-200 font-bold rounded-lg' onClick={this.preClickHandler}>&larr; Previous</button>
           <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className='py-2 px-4 bg-zinc-800 text-zinc-200 font-bold rounded-lg' onClick={this.nextClickHandler}>Next &rarr;</button>
-        </div>
+        </div> */}
       </>
     )
   }
